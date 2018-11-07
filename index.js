587e534e3c87616ecd680efc05acd7404a047d53
Anton Kosykh <kelin2025@yandex.ru>
2018-11-07 03:48:58 +0300

    Added 'once' option

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const createWaiter = name => {
   const cbs = []
+  const params = []
 
-  const waitAndGo = (cb, params) => {
+  const waitAndGo = (cb, p) => {
     cbs.push(cb)
+    params.push(p || { once: false })
   }
 
   const resolve = payload => {
-    cbs.forEach(cb => cb(payload))
+    cbs.forEach((cb, idx) => {
+      cb(payload)
+      if (params[idx].once) {
+        cbs.splice(idx, 1)
+        params.splice(idx, 1)
+      }
+    })
   }
 
   waitAndGo.do = cb => {
